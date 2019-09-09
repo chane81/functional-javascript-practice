4f4d8df96b66287d7b298b6e6695f5bee090995d
BM <chane81@naver.com>
2019-09-09 23:50:54 +0900

    curry, map, filter, reduce 함수모음

--- 5. 코드를 값으로 다루어 표현력 높이기/fx.js
+++ 5. 코드를 값으로 다루어 표현력 높이기/fx.js
@@ -1,35 +1,47 @@
 const log = console.log;
 
-const line = arg => console.log(arg + '===========================');
+const line = arg =>
+	console.log(
+		arg + '==========================='
+	);
 
+// curry
+const curry = f => (..._) =>
+	f.length === _.length
+		? f(..._)
+		: (...nextArgs) =>
+				currySecond(f)(..._, ...nextArgs);
+
+// map
 const map = (f, iter) => {
-  let res = [];
-  for (const p of iter) {
-    res.push(f(p));
-  }
+	let res = [];
+	for (const p of iter) {
+		res.push(f(p));
+	}
 
-  return res;
+	return res;
 };
 
+// filter
 const filter = (f, iter) => {
-  let res = [];
-  for (const a of iter) {
-    if (f(a)) res.push(a);
-  }
+	let res = [];
+	for (const a of iter) {
+		if (f(a)) res.push(a);
+	}
 
-  return res;
+	return res;
 };
 
+// reduce
 const reduce = (f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  }
+	if (!iter) {
+		iter = acc[Symbol.iterator]();
+		acc = iter.next().value;
+	}
 
-  for (const a of iter) {
-    acc = f(acc, a);
-  }
+	for (const a of iter) {
+		acc = f(acc, a);
+	}
 
-  return acc;
+	return acc;
 };
-
