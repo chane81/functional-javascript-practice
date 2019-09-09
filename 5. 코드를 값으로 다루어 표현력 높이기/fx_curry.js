8c54aac20b3ac651f2006695cb2684e808e90cf3
BM <chane81@naver.com>
2019-09-09 23:51:07 +0900

    각 함수에 curry 적용

---
+++ 5. 코드를 값으로 다루어 표현력 높이기/fx_curry.js
@@ -0,0 +1,51 @@
+const log = console.log;
+
+const line = arg =>
+	console.log(
+		arg + '==========================='
+	);
+
+// curry
+const curry = f => (..._) =>
+	f.length === _.length
+		? f(..._)
+		: (...nextArgs) =>
+				curry(f)(..._, ...nextArgs);
+
+// map
+const map = curry((f, iter) => {
+	let res = [];
+	for (const p of iter) {
+		res.push(f(p));
+	}
+
+	return res;
+});
+
+// filter
+const filter = curry((f, iter) => {
+	let res = [];
+	for (const a of iter) {
+		if (f(a)) res.push(a);
+	}
+
+	return res;
+});
+
+// reduce
+const reduce = curry((f, iter) => {
+	iter = iter[Symbol.iterator]();
+
+	const acc = iter.next().value;
+
+	return reduceOrg(f, acc, iter);
+});
+
+// reduce original
+const reduceOrg = curry((f, acc, iter) => {
+	for (const a of iter) {
+		acc = f(acc, a);
+	}
+
+	return acc;
+});
